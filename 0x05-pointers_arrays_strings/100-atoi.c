16672b15f0fff4bdcfbb7f74c9ca175409b81339
Marnuchoe <olubolajidamilola@gmail.com>
2023-02-27 15:44:20 -0800

    pointersarraysstrings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,21 +10,14 @@
 int _atoi(char *s)
 
 {
-
 	int i, d, n, len, f, digit;
 
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
-
 
 	while (s[len] != '\0')
 
@@ -43,15 +36,12 @@ int _atoi(char *s)
 		{
 
 			digit = s[i] - '0';
-
 			if (d % 2)
 
 				digit = -digit;
 
 			n = n * 10 + digit;
-
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 
 				break;
@@ -60,15 +50,9 @@ int _atoi(char *s)
 
 		}
 
-		i++;
-
+			i++;
 	}
-
-
-	if (f == 0)
-
-	return (0);
-
+		if (f == 0)
+		return (0);
 	return (n);
-
 }
